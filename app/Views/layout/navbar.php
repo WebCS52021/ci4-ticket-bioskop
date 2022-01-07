a0e364b0eef16de271b8ba05c6b110c530be464c
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2022-01-07 07:25:42 +0700

    feat: add component view navbar

---
+++ app/Views/layout/navbar.php
@@ -0,0 +1,13 @@
+<header>
+    <nav class="bg-violet-500 text-white">
+        <ul class="flex justify-between items-center px-5 py-2">
+            <li><a href="#">Logo</a></li>
+            <ul class="flex justify-between">
+                <a class="px-5 py-2 hover:bg-violet-400 rounded-lg" href="/">Home</a>
+                <a class="px-5 py-2 hover:bg-violet-400 rounded-lg" href="/pages/catalog">Catalog</a>
+                <a class="px-5 py-2 hover:bg-violet-400 rounded-lg" href="/pages/about">About</a>
+            </ul>
+            <Button class="bg-violet-700 text-white py-2 px-4 rounded-md">Login</Button>
+        </ul>
+    </nav>
+</header>
