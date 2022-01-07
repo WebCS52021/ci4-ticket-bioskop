25b6c9830ac1f0735fae8b49ce4ee9b609a5450a
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2022-01-07 07:26:05 +0700

    feat: add controller for pages

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -8,6 +8,28 @@ class Pages extends BaseController
 {
     public function index()
     {
-        //
+        $data = [
+            'title'=> 'Home',
+        ];
+
+        return view('pages/home', $data);
+    }
+
+    public function about()
+    {
+        $data = [
+            'title'=> 'About',
+        ];
+
+        return view('pages/about', $data);
+    }
+
+    public function catalog()
+    {
+        $data = [
+            'title'=> 'Catalog',
+        ];
+
+        return view('pages/catalog', $data);
     }
 }
