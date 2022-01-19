e8a988bb0bbe2cbded8af10e99e7bed890aa70e8
ahannazara <notmicrowings676@gmail.com>
2022-01-19 09:29:24 +0700

    feat: penambahan sidebar

--- app/Views/pages/recomend.php
+++ app/Views/pages/recomend.php
@@ -1,163 +1,225 @@
 <?= $this->extend('layout/template') ;?>
 
 <?= $this->section('content') ;?>
-
-
-<h1 class="h1recomend violet_recomend"><b>RECOMENDED FOR YOU</b></h1>
-
-
-<h2 class="h1recomend violet_recomend"><a href="#action">ACTION</a></h2>
-<h2 class="h1recomend violet_recomend"><a href="#romance">ROMANCE</a></h2>
-<h2 class="h1recomend violet_recomend"><a href="#comedy">COMEDY</a></h2>
-<h2 class="h1recomend violet_recomend"><a href="#scifi">SCI-FI</a></h2>
-
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<div id="action" class="float-child bg-violet-500" name="action">
-<p> </p>
-<h1 class="h1recomend white_recomend"><b>ACTION</b></h1>
-<p> </p>
-</div>
-<div>
-
-</div>
-<div id="romance" class="float-child bg-violet-500" name="romance">
-<p> </p>
-<h1 class="h1recomend white_recomend"><b>ROMANCE</b></h1>
-<p> </p>
-</div>
-
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+<div id="to_top">
 <br>
 </div>
-<div id="comedy" class="float-child bg-violet-500" name="comedy">
-<p> </p>
-<h1 class="h1recomend white_recomend"><b>COMEDY</b></h1>
-<p> </p>
+<?php
+////////////////////////////////////////////////////RECOMEND CODE START///////////////////////////////////////  
+?>
+<div class="bg-violet-500" name="">
+    <p> </p>
+    <h1 class="h1recomend white_recomend"><b>TRENDING</b></h1>
+    <p> </p>
 </div>
 
-</div>
-<div id="scifi" class="float-child bg-violet-500" name="scifi">
-<p> </p>
-<h1 class="h1recomend white_recomend"><b>SCI-FI</b></h1>
-<p> </p>
+    <h1 class="h1recomend violet_recomend"><b>RECOMENDED FOR YOU</b></h1>
+
+<?php
+////////////////////////////////////////////////////RECOMEND CODE END///////////////////////////////////////  
+?>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+   
+<?php
+////////////////////////////////////////////////////GENRE CODE START///////////////////////////////////////  
+?>
+<div class="sidenav_recomend">
+<a class="h1recomend violet_recomend"><b>POPULAR GENRE</b></a>
+<a class="h1recomend violet_recomend" href="#action">ACTION</a>
+<a class="h1recomend violet_recomend" href="#romance">ROMANCE</a>
+<a class="h1recomend violet_recomend" href="#comedy">COMEDY</a>
+<a class="h1recomend violet_recomend" href="#scifi">SCI-FI</a>
 </div>
 
 
+<?php
+////////////////////////////////////////////////////GENRE CODE END///////////////////////////////////////  
+?>
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<?php
+////////////////////////////////////////////////////GENRE RECOMENDED GENRE START///////////////////////////////////////  
+?>
+<div class="float-container">
+    <div id="action" class="float-child bg-violet-500" name="action">
+        <p> </p>
+        <h1 class="h1recomend white_recomend"><b>ACTION</b></h1>
+        <p> </p>
+    </div>
+
+
+    <div id="romance" class="float-child bg-violet-500" name="romance">
+        <p> </p>
+        <h1 class="h1recomend white_recomend"><b>ROMANCE</b></h1>
+        <p> </p>
+    </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="float-container">
+
+    <div id="comedy" class="float-child bg-violet-500" name="comedy">
+        <p> </p>
+        <h1 class="h1recomend white_recomend"><b>COMEDY</b></h1>
+        <p> </p>
+    </div>
+
+
+    <div id="scifi" class="float-child bg-violet-500" name="scifi">
+        <p> </p>
+        <h1 class="h1recomend white_recomend"><b>SCI-FI</b></h1>
+        <p> </p>
+    </div>
+    
+</div>
+
 
 <br>
 <br>
@@ -189,8 +251,11 @@
 <br>
 <br>
 
+<?php
+////////////////////////////////////////////////////GENRE RECOMENDED GENRE START///////////////////////////////////////  
+?>
 
-<a href="#To top">Scroll to top</a>
+<a href="#to_top">Scroll to top</a>
 </div>
 
 
