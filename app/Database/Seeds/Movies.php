20bd6b462130bc8d5eb6eb44d9e18bf9c8eff8f4
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2022-01-19 22:52:35 +0700

    chore: add multiple data to seed database

--- app/Database/Seeds/Movies.php
+++ app/Database/Seeds/Movies.php
@@ -26,6 +26,33 @@ class Movies extends Seeder
                 'movie_genre' => "action, comedy, historical",
                 'movie_rating' => 4.8,
                 'movie_price' => "Rp. 30.000"
+            ],
+            [
+                'movie_title' => 'Dear Nathan2 - Thank you salma',
+                'slug' => 'dear-nathan-2',
+                'movie_thumbnail' => "dear-nathan-2.jpeg",
+                'movie_desc' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere unde porro cumque inventore consequuntur pariatur rerum voluptates reprehenderit fugit, placeat dolores? Sequi minus deserunt aliquam repudiandae at harum ad unde ullam totam laudantium inventore dicta doloremque illum voluptate exercitationem, perspiciatis illo ea eos dolores autem veniam fuga sint! Ipsam temporibus ullam nostrum unde, ipsum dolorem id perferendis. Asperiores, suscipit esse nulla, nam quasi, odit ea consequuntur et iusto deleniti eligendi! Et provident nihil illo distinctio voluptatibus placeat quia perspiciatis, non ipsa itaque ut quidem, deleniti repellendus est? Dolorem magni, ab, quidem facilis optio nisi vero laborum inventore delectus dolores ex.',
+                'movie_genre' => "romance, comedy",
+                'movie_rating' => 4.1,
+                'movie_price' => "Rp. 25.000"
+            ],
+            [
+                'movie_title' => 'Cinta Pertama, Kedua dan Ketiga',
+                'slug' => 'cinta-pertama-2-3',
+                'movie_thumbnail' => "cinta-pertama.jpg",
+                'movie_desc' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere unde porro cumque inventore consequuntur pariatur rerum voluptates reprehenderit fugit, placeat dolores? Sequi minus deserunt aliquam repudiandae at harum ad unde ullam totam laudantium inventore dicta doloremque illum voluptate exercitationem, perspiciatis illo ea eos dolores autem veniam fuga sint! Ipsam temporibus ullam nostrum unde, ipsum dolorem id perferendis. Asperiores, suscipit esse nulla, nam quasi, odit ea consequuntur et iusto deleniti eligendi! Et provident nihil illo distinctio voluptatibus placeat quia perspiciatis, non ipsa itaque ut quidem, deleniti repellendus est? Dolorem magni, ab, quidem facilis optio nisi vero laborum inventore delectus dolores ex.',
+                'movie_genre' => "romance, comedy",
+                'movie_rating' => 4.0,
+                'movie_price' => "Rp. 25.000"
+            ],
+            [
+                'movie_title' => 'Makmum 2',
+                'slug' => 'makmum-2',
+                'movie_thumbnail' => "makmum-2.jpeg",
+                'movie_desc' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere unde porro cumque inventore consequuntur pariatur rerum voluptates reprehenderit fugit, placeat dolores? Sequi minus deserunt aliquam repudiandae at harum ad unde ullam totam laudantium inventore dicta doloremque illum voluptate exercitationem, perspiciatis illo ea eos dolores autem veniam fuga sint! Ipsam temporibus ullam nostrum unde, ipsum dolorem id perferendis. Asperiores, suscipit esse nulla, nam quasi, odit ea consequuntur et iusto deleniti eligendi! Et provident nihil illo distinctio voluptatibus placeat quia perspiciatis, non ipsa itaque ut quidem, deleniti repellendus est? Dolorem magni, ab, quidem facilis optio nisi vero laborum inventore delectus dolores ex.',
+                'movie_genre' => "horror, thriller",
+                'movie_rating' => 4.5,
+                'movie_price' => "Rp. 25.000"
             ]
         ];
 
