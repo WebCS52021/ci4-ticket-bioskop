51c90940830527cb398acdba71e45ba2f89ae628
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2022-01-26 17:02:31 +0700

    feat: change seed movies

--- app/Database/Seeds/Movies.php
+++ app/Database/Seeds/Movies.php
@@ -16,7 +16,7 @@ class Movies extends Seeder
                 'movie_desc' => 'Film ini merupakan awal dari berkumpulnya para avangers untuk bla bla bla',
                 'movie_genre' => "action, comedy, romances",
                 'movie_rating' => 4.5,
-                'movie_price' => "Rp. 35.000"
+                'movie_price' => "35000"
             ],
             
             [
@@ -26,7 +26,7 @@ class Movies extends Seeder
                 'movie_desc' => 'Film ini merupakan awal dari berkumpulnya para spiderman di universe lain untuk bla bla bla',
                 'movie_genre' => "action, comedy, historical",
                 'movie_rating' => 4.8,
-                'movie_price' => "Rp. 30.000"
+                'movie_price' => "30000"
             ],
             [
                 'movie_title' => 'Dear Nathan2 - Thank you salma',
@@ -35,7 +35,7 @@ class Movies extends Seeder
                 'movie_desc' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere unde porro cumque inventore consequuntur pariatur rerum voluptates reprehenderit fugit, placeat dolores? Sequi minus deserunt aliquam repudiandae at harum ad unde ullam totam laudantium inventore dicta doloremque illum voluptate exercitationem, perspiciatis illo ea eos dolores autem veniam fuga sint! Ipsam temporibus ullam nostrum unde, ipsum dolorem id perferendis. Asperiores, suscipit esse nulla, nam quasi, odit ea consequuntur et iusto deleniti eligendi! Et provident nihil illo distinctio voluptatibus placeat quia perspiciatis, non ipsa itaque ut quidem, deleniti repellendus est? Dolorem magni, ab, quidem facilis optio nisi vero laborum inventore delectus dolores ex.',
                 'movie_genre' => "romance, comedy",
                 'movie_rating' => 4.1,
-                'movie_price' => "Rp. 25.000"
+                'movie_price' => "25000"
             ],
             [
                 'movie_title' => 'Cinta Pertama, Kedua dan Ketiga',
@@ -44,7 +44,7 @@ class Movies extends Seeder
                 'movie_desc' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere unde porro cumque inventore consequuntur pariatur rerum voluptates reprehenderit fugit, placeat dolores? Sequi minus deserunt aliquam repudiandae at harum ad unde ullam totam laudantium inventore dicta doloremque illum voluptate exercitationem, perspiciatis illo ea eos dolores autem veniam fuga sint! Ipsam temporibus ullam nostrum unde, ipsum dolorem id perferendis. Asperiores, suscipit esse nulla, nam quasi, odit ea consequuntur et iusto deleniti eligendi! Et provident nihil illo distinctio voluptatibus placeat quia perspiciatis, non ipsa itaque ut quidem, deleniti repellendus est? Dolorem magni, ab, quidem facilis optio nisi vero laborum inventore delectus dolores ex.',
                 'movie_genre' => "romance, comedy",
                 'movie_rating' => 4.0,
-                'movie_price' => "Rp. 25.000"
+                'movie_price' => "25000"
             ],
             [
                 'movie_title' => 'Makmum 2',
@@ -53,7 +53,7 @@ class Movies extends Seeder
                 'movie_desc' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere unde porro cumque inventore consequuntur pariatur rerum voluptates reprehenderit fugit, placeat dolores? Sequi minus deserunt aliquam repudiandae at harum ad unde ullam totam laudantium inventore dicta doloremque illum voluptate exercitationem, perspiciatis illo ea eos dolores autem veniam fuga sint! Ipsam temporibus ullam nostrum unde, ipsum dolorem id perferendis. Asperiores, suscipit esse nulla, nam quasi, odit ea consequuntur et iusto deleniti eligendi! Et provident nihil illo distinctio voluptatibus placeat quia perspiciatis, non ipsa itaque ut quidem, deleniti repellendus est? Dolorem magni, ab, quidem facilis optio nisi vero laborum inventore delectus dolores ex.',
                 'movie_genre' => "horror, thriller",
                 'movie_rating' => 4.5,
-                'movie_price' => "Rp. 25.000"
+                'movie_price' => "25000"
             ]
         ];
 
