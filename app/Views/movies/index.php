ac5070b6f9c8f54974b5871a93e545415e753c0d
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2022-01-26 17:05:32 +0700

    feat: add navbar

--- app/Views/movies/index.php
+++ app/Views/movies/index.php
@@ -1,6 +1,8 @@
 <?= $this->extend('layout/template'); ?>
 
 <?= $this->section('content'); ?>
+<!-- Navbar -->
+<?= $this->include('layout/navbar') ;?>
 
 <div class="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 mx-auto">
     <?php foreach ($movies as $movie) : ?>
